32427fa94554c55631e6dbcb9eaaaa73456a8d7f
pavelrysev <lawyerrysevpp@gmail.com>
2023-04-17 00:59:36 +0300

    17.04.2023 00-59

---
+++ lesson_2/ДЗ_3_shapka.py
@@ -0,0 +1,38 @@
+"""
+дисциплина: "Наука данных для юристов"
+домашнее задание № 1 по теме № 1 "Введение в Python"
+
+студент: Павел Петрович Рысев
+группа ВШЭ|Нетология МЛТ221
+"""
+
+import lesson_2_data
+
+def shapka(dict_sh):
+    if "case_number" in dict_sh:
+        sud_number = dict_sh["case_number"].split("-")[0]
+        for item in lesson_2_data.courts:
+            if sud_number == item['court_code']:
+                sud = item['court_name']
+                adress = item['court_address']
+        print(f"""
+        В {sud}
+        Адрес: {adress}
+        
+        Истец: Рысев Павел Петрович
+        ИНН: 123456789001
+        Адрес: 121351, г. Москва, ул. Крамолы, д. 7, кв. 121
+        
+        Ответчик: {dict_sh['short_name']}
+        ИНН: {dict_sh['inn']}, ОГРН: {dict_sh['ogrn']}
+        Адрес: {dict_sh['address']}
+        
+        Дело № {dict_sh["case_number"]}
+        """)
+
+
+def perebor(dict_sh_1):
+    for item in dict_sh_1:
+        shapka(item)
+
+perebor(lesson_2_data.repondents)
